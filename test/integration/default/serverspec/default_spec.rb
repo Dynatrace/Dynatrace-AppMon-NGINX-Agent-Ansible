a065dc7b8ccfdf4f1cfb5032f93adf0ebaea23d1
Martin Etmajer <m.etmajer@gmail.com>
2015-09-30 19:41:26 +0200

    Fix NGINX agent integration.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,6 +3,6 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/tmp/nginx.conf') do
-  its(:content) { should match /^envLD_PRELOAD="\/opt\/dynatrace\/agent\/lib64\/libdtagent\.so";$/ }
+describe file('/etc/default/nginx') do
+  its(:content) { should match /^export LD_PRELOAD="\/opt\/dynatrace\/agent\/lib64\/libdtagent\.so"$/ }
 end
